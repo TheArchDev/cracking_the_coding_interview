0ba4152d8eb7362ae6daa4f24fd8f4d394f8a89b
Alex Archer <archeralex@gmail.com>
2016-04-26 12:25:21 +0100

    Minor improvement, removing base/initial case from for loop

--- arrays_and_strings/question_1-5.py
+++ arrays_and_strings/question_1-5.py
@@ -1,13 +1,37 @@
+##Initial solution
+# string = raw_input()
+# temp = ''
+# count = 0
+# output = ''
+
+# for x in range(len(string)):
+# 	if x == 0:
+# 		temp = string[x]
+# 		count += 1
+# 	elif string[x] == temp:
+# 		count += 1
+# 	else:
+# 		output += temp + str(count)
+# 		temp = string[x]
+# 		count = 1
+
+# output += temp + str(count)
+
+# if len(output) < len(string):
+# 	print output
+# else:
+# 	print string
+
+#Improvement to solution, after checking first book solution
+#Taking base/initial case out of the for loop
+
 string = raw_input()
-temp = ''
-count = 0
+temp = string[0]
+count = 1
 output = ''
 
-for x in range(len(string)):
-	if x == 0:
-		temp = string[x]
-		count += 1
-	elif string[x] == temp:
+for x in range(1,len(string)):
+	if string[x] == temp:
 		count += 1
 	else:
 		output += temp + str(count)
